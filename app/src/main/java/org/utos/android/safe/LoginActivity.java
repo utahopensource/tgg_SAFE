5f98ffc7fa21ea9403e927c26a410b5b4b64fa27
Scott Knutti <sknutti@gmail.com>
2017-01-14 12:28:54 -0700

    Made the buttons circular and the urgent button bigger

--- app/src/main/java/org/utos/android/safe/LoginActivity.java
+++ app/src/main/java/org/utos/android/safe/LoginActivity.java
@@ -3,6 +3,7 @@ package org.utos.android.safe;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,7 +334,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
