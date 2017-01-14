e1f6ffba957b5872b89ba1b71d3e8303d66af704
Chris McAfee <Chris McAfee>
2017-01-14 14:45:40 -0700

    Caseworker and user language selected in startup screen are saved to the SharedPreferences

--- app/src/main/java/org/utos/android/safe/SetupActivity.java
+++ app/src/main/java/org/utos/android/safe/SetupActivity.java
@@ -1,6 +1,7 @@
 package org.utos.android.safe;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,23 +14,19 @@ import android.widget.Spinner;
 
 public class SetupActivity extends AppCompatActivity {
 
+    // these strings are keys to access the caseworker and user language values in SharedPrefs
+    public static String CASE_WORKER = "caseWorker";
+    public static String USER_LANG = "userLang";
+    public static String SHARED_PREFS = "sharedPrefsFile";
+
     private Spinner mCaseWorkerSpinner;
     private Spinner mLanguageSpinner;
-
-//    Spinner spinner = (Spinner) findViewById(R.id.spinner);
-//    // Create an ArrayAdapter using the string array and a default spinner layout
-//    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-//            R.array.planets_array, android.R.layout.simple_spinner_item);
-//    // Specify the layout to use when the list of choices appears
-//    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//    // Apply the adapter to the spinner
-//    spinner.setAdapter(adapter);
-//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        //TODO: pull caseworkers and supported langs from DB
 
         mCaseWorkerSpinner = (Spinner) findViewById(R.id.spinner_caseworker);
         ArrayAdapter<CharSequence> caseWorkerAdapter = ArrayAdapter.createFromResource(this,
@@ -57,10 +54,20 @@ public class SetupActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
+    /**
+     * When continue button is clicked the selected case worker and language are stored in Shared Preferences
+     * and the Login activity is launched
+     * @param view
+     */
     public void continueClicked(View view) {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, 0);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString(CASE_WORKER, mCaseWorkerSpinner.getSelectedItem().toString());
+        prefsEditor.putString(USER_LANG, mLanguageSpinner.getSelectedItem().toString());
+        prefsEditor.commit();
+
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
